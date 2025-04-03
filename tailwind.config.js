bdf0542ed4e1b201b78eed4888436899c34a40ec
My project <singhamit18033@gmail.com>
2025-04-03 22:47:50 +0530

    dasisy chamge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [daisyui],
+}
